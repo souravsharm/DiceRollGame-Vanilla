897d4ed4f8318f01696424e5b1773e8917855cde
souravsharma <msg2souravsharma@gmail.com>
2025-02-12 14:54:23 +1100

    displayed the winner

--- index.js
+++ index.js
@@ -10,5 +10,17 @@ buttonClickEventHandler.onclick = () => {
 
     document.getElementById("img1").setAttribute("src", urlImg(random1))
     document.getElementById("img2").setAttribute("src", urlImg(random2))
+    
+    if(random1 > random2){
+        document.querySelector("h1").innerText = "Player 1 wins!"
+    }
+    else if(random2 === random1){
+        document.querySelector("h1").innerText = "Its a Draw!"
+
+    }else{
+        document.querySelector("h1").innerText = "Player 2 wins!"
+
+    }
 
 }
+
